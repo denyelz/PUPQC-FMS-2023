b45e7d4fccde5d25bdcde8420bdcec7f417297a7
AJBrinas <drewbrinas@gmail.com>
2023-05-02 05:37:48 +0800

    on.

---
+++ testing/laravel/daniel_draft_1.2/resources/views/testing ulet/dist/js/farms.swal.success.modal.js
@@ -0,0 +1,17 @@
+  $(function() {
+    var Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 4000
+    });
+
+    
+
+    $('.swalDefaultSuccess').click(function() {
+      Toast.fire({
+          icon: "success",
+          title: "Successfully created a new role!",
+      })
+    });
+  });
